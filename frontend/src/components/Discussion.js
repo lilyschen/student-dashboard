fcfe4f105ed094759de61685bd3c1b3c481ade58
lilyschen <lily.s.chen95@gmail.com>
2024-10-18 15:08:29 -0700

    steps 3 and 4

--- frontend/src/components/Discussion.js
+++ frontend/src/components/Discussion.js
@@ -6,6 +6,11 @@ function Discussion () {
   const [timestamps, setTimestamps] = useState([])
 
   // add useEffect here for discussion
+  useEffect(() => {
+    fetch('http://localhost:4001/getDiscussions')
+      .then(res => res.json())
+      .then(data => setDiscussion(data))
+  }, [])
 
   useEffect(() => {
     if (discussion.length > 0) {
