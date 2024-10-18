02283aa73a04002a94622f6ed72658b5010da2e7
lilyschen <lily.s.chen95@gmail.com>
2024-10-18 14:40:52 -0700

    steps 1 and 2

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,26 @@ app.use(cors())
 app.get('/', (req, res) => res.send('Hello World!'))
 
 // Make API call to Canvas API here
+canvasAPI.getSelf()
+    .then(self => console.log(self))
 
 
 // Make endpoint for getSelf here
+app.get('/getSelf', async (req, res) => {
+    // we need to now make a call to the Canvas API,
+    // wait for the response, then send the result to the frontend
+    const self = await canvasAPI.getSelf()
+    res.json(self)
+})
 
 
 // Make endpoint for getDiscussions here
+app.get('/getDiscussions', async (req, res) => {
+    const courseId = 161721
+    const discussions = await getDiscussions(courseId)
+    const formattedDiscussions = flattenTopicAndReplies(discussions)
+    res.json(formattedDiscussions)
+})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
